92a2b775ae59c99eeff35fb0e239bb30e9ee918a
gilbert <sdv71433@gmail.com>
2026-05-02 18:11:00 +0300

    Add KeypadPhone derived class

---
+++ src/main/java/org/lab4/KeypadPhone.java
@@ -0,0 +1,18 @@
+package org.lab4;
+
+/**
+ * Клас Кнопковий телефон, що успадковує Phone.
+ */
+public class KeypadPhone extends Phone {
+    private boolean hasFlashlight;
+
+    public KeypadPhone(String brand, String model, double price, boolean hasFlashlight) {
+        super(brand, model, price);
+        this.hasFlashlight = hasFlashlight;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" | Кнопковий [Ліхтарик: %s]", hasFlashlight ? "Так" : "Ні");
+    }
+}
