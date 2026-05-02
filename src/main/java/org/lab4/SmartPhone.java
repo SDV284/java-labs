dd8432371d0ae4cc112f2b5702e12b0ceaee41b0
gilbert <sdv71433@gmail.com>
2026-05-02 18:10:51 +0300

    Add SmartPhone derived class

---
+++ src/main/java/org/lab4/SmartPhone.java
@@ -0,0 +1,21 @@
+package org.lab4;
+
+/**
+ * Клас Смартфон, що успадковує Phone.
+ */
+public class SmartPhone extends Phone {
+    private double screenSize;
+    private String os;
+
+    public SmartPhone(String brand, String model, double price, double screenSize, String os) {
+        super(brand, model, price); // Виклик конструктора базового класу
+        this.screenSize = screenSize;
+        this.os = os;
+    }
+
+    @Override
+    public String toString() {
+        // Поліморфне розширення методу toString
+        return super.toString() + String.format(" | Смартфон [Екран: %.1f\", ОС: %s]", screenSize, os);
+    }
+}
