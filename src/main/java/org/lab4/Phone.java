3ce8a4fb0406e966c48ec59a11cd4bf823f770b4
gilbert <sdv71433@gmail.com>
2026-05-02 16:18:02 +0300

    Add Phone class with logic

---
+++ src/main/java/org/lab4/Phone.java
@@ -0,0 +1,68 @@
+package org.lab4;
+
+import java.util.Objects;
+
+/**
+ * Клас Phone представляє модель мобільного телефону.
+ * Містить інформацію про бренд, модель та ціну.
+ *
+ * @author Ваше Прізвище
+ * @version 1.0
+ */
+public class Phone {
+    private String brand;
+    private String model;
+    private double price;
+
+    /**
+     * Конструктор з параметрами.
+     * @param brand бренд телефону
+     * @param model модель телефону
+     * @param price ціна телефону
+     */
+    public Phone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    // Гетери та сетери
+    public String getBrand() { return brand; }
+    public void setBrand(String brand) { this.brand = brand; }
+
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    /**
+     * Повертає текстове представлення об'єкта.
+     */
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    /**
+     * Порівнює поточний об'єкт з іншим на рівність за всіма полями.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Phone phone = (Phone) o;
+        return Double.compare(phone.price, price) == 0 &&
+                Objects.equals(brand, phone.brand) &&
+                Objects.equals(model, phone.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, price);
+    }
+}
