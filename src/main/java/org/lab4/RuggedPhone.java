1b4cef7ae922d0ddafeefa5ac7738b4ddbe0124d
gilbert <sdv71433@gmail.com>
2026-05-07 11:53:17 +0300

    Add RuggedPhone class extending Phone

---
+++ src/main/java/org/lab4/RuggedPhone.java
@@ -0,0 +1,15 @@
+package org.lab4;
+
+public class RuggedPhone extends Phone {
+    private int ipRating;
+
+    public RuggedPhone(String brand, String model, double price, int ipRating) {
+        super(brand, model, price);
+        this.ipRating = ipRating;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" | Захищений [IP Рейтинг: IP%d]", ipRating);
+    }
+}
