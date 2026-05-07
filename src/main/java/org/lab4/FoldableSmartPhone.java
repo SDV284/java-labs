3019e3db25b69c869b3187040b0f3ebc3ed43448
gilbert <sdv71433@gmail.com>
2026-05-07 11:53:26 +0300

    Add FoldableSmartphone extending SmartPhone

---
+++ src/main/java/org/lab4/FoldableSmartPhone.java
@@ -0,0 +1,15 @@
+package org.lab4;
+
+public class FoldableSmartPhone extends SmartPhone {
+    private int foldingCycles; // Гарантована кількість циклів складання
+
+    public FoldableSmartPhone(String brand, String model, double price, double screenSize, String os, int foldingCycles) {
+        super(brand, model, price, screenSize, os);
+        this.foldingCycles = foldingCycles;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" | Складаний [Ресурс згинання: %d циклів]", foldingCycles);
+    }
+}
